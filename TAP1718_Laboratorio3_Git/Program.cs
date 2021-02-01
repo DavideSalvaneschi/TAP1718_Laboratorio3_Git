635cbefec9e271f23080235103599fcc7bb173bb
Davide Salvaneschi <S3378553@studenti.unige.it>
2021-02-01 20:11:29 +0200

    rafiki stopped with the nonsense

--- TAP1718_Laboratorio3_Git/Program.cs
+++ TAP1718_Laboratorio3_Git/Program.cs
@@ -7,7 +7,7 @@ namespace TAP1718_Laboratorio3_Git
         public static void Main(string[] args)
         {
             Console.WriteLine("Se leggete questo messaggio, significa che siete tornati indietro nel tempo!");
-            Console.WriteLine("Asante sana cocco banana uikki ukki uikki wa. LUI VIVE IN TE!!");
+            Console.WriteLine("Asante sana cocco banana. LUI VIVE IN TE!!");
 
         }
     }
